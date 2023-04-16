2d7026c14ff45eec0d07fbdcc2acc2f1f7a10d3d
uswitch-02 <chunxidubian64@gmail.com>
2023-04-16 05:59:48 +0000

    [update]2-1-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user = current_user
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
