398fbe02f72a73593512ef29a326ccb5849d6471
uswitch-02 <chunxidubian64@gmail.com>
2023-04-20 02:21:46 +0000

    [update]2-1-3ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
-
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
-
+  resources :books do
+      resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
